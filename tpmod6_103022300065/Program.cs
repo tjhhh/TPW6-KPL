48540bea9b1b4ccde4ee648c516823bed1d81255
tjhhh <muhammadfauzan292004@gmail.com>
2025-03-23 21:06:50 +0700

    menambahkan class SayaTubeVideo

--- tpmod6_103022300065/Program.cs
+++ tpmod6_103022300065/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using tpmod6_103022300065;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SayaTubeVideo video1 = new SayaTubeVideo("Tutorial Design By Contract - [MUHAMMAD FAUZAN]");
+        video1.IncreasePlayCount();
+        video1.IncreasePlayCount();
+        video1.PrintVideoDetails();
+    }
+}
---
+++ tpmod6_103022300065/SayaTubeVideo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmod6_103022300065
+{
+    class SayaTubeVideo
+    {
+        private int id;
+        private String title;
+        private int playCount;
+
+        public SayaTubeVideo(String title)
+        {
+            this.id = new Random().Next(10000, 99999);
+            this.title = title;
+            this.playCount = 0;
+        }
+        public void IncreasePlayCount()
+        {
+            this.playCount++;
+        }
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("Video ID: " + this.id);
+            Console.WriteLine("Title: " + this.title);
+            Console.WriteLine("Play Count: " + this.playCount);
+        }
+    }
+}
